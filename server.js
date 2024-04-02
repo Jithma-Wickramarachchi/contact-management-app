47f0c8944dd273685b5c8addb04efa9f61f39756
Jithma Wickramarachchi <151661874+ProphetAlphaX@users.noreply.github.com>
2024-04-02 13:58:51 +0530

    Setup CRUD routing using Express router

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000 ;
+
+app.use("/api/contacts", require("../contact-management-app/routes/contactRoutes"))
 
 app.listen(port, () => {
     console.log(`App runs on port ${port}`)
